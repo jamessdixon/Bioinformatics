692c990312b9d5e884b0ce62f72e446eb04dfbee
Jamie Dixon <jamessdixon@gmail.com>
2020-09-18 11:04:39 +0000

    for real this time

--- QuestionOne.fsx
+++ QuestionOne.fsx
@@ -1,3 +1,12 @@
 
-let foo = "bar"
-printfn "%s" foo
+let patternCount (text:string) (pattern:string) =
+    text 
+    |> Seq.windowed pattern.Length 
+    |> Seq.map(fun c -> new string(c))
+    |> Seq.filter(fun s -> s = pattern)
+    |> Seq.length
+
+let text = "ACAACTCTGCATACTATCGGGAACTATCCT"
+let pattern = "ACTAT"
+
+patternCount text pattern
